61a89d7bad676d0041b57da2489c9a99477aff77
SaqeefShabbir <saqeef2016@gmail.com>
2025-07-04 21:01:14 +0500

    setup application

---
+++ smart-solutions-spring-boot/src/main/java/com/smartsolutions/smart_solutions/SmartSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.smartsolutions.smart_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSolutionsApplication.class, args);
+	}
+
+}
