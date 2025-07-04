c468e9b59eef61903905dcfcb2b75e0e3515e878
SaqeefShabbir <saqeef2016@gmail.com>
2025-07-04 21:10:13 +0500

    add tests

---
+++ smart-solutions-spring-boot/src/test/java/com/smartsolutions/smart_solutions/SmartSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsolutions.smart_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
